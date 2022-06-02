6b3cbbd0892bd5a21bb0bdaed429fb46e273732b
HuangYun <hy020316@126.com>
2022-06-03 01:26:37 +0800

    对系统文件进行更改

--- shop/settings.py
+++ shop/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# 当前项目的系统路径
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -8,16 +10,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# 密钥配置，无需修改——提高安全度
 SECRET_KEY = 'django-insecure-ahf6gl28_gpfa_dfiqk8i6o&tmo!^^z*8#$q&7r@r%gwp1f@6h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# 调试模式
 DEBUG = True
 
+# 设置可访问的域名，DEBUG为False时为必填项，
+# 要设置所有域名可访问，可设置成 ['*']
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# App列表，告诉Django有哪些App
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,9 +31,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index',
+    'commodity',
+    'shopper'
 
 ]
-
+# 中间件
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -37,9 +46,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+# 路由入口设置
 ROOT_URLCONF = 'shop.urls'
-
+# 模板配置，配置模板的解析引擎、存放地址等信息
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -56,13 +65,13 @@ TEMPLATES = [
         },
     },
 ]
-
+# 告诉Django如何查找WSGI文件
 WSGI_APPLICATION = 'shop.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# 数据库文件，配置数据的连接信息
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -73,7 +82,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
+# 实现Django的Auth认证系统功能
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
