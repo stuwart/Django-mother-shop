24eaed89b452701071c0921aa59c2e5e6accaa61
HuangYun <hy020316@126.com>
2022-06-04 13:26:27 +0800

    4.4.3

--- commodity/models.py
+++ commodity/models.py
@@ -4,21 +4,22 @@ from django.db import models
 # Create your models here.
 
 class Types(models.Model):
-    id = models.AutoField(primary_key=True)       #自增长类型.    primary_key：设置主键
-    firsts = models.CharField('一级类型', max_length=100)  #Verbose_name:默认为None，在Admin站点管理设置字段的显示名称。
+    id = models.AutoField(primary_key=True)  # 自增长类型.    primary_key：设置主键
+    firsts = models.CharField('一级类型', max_length=100)  # Verbose_name:默认为None，在Admin站点管理设置字段的显示名称。
     seconds = models.CharField('二级类型', max_length=100)
 
-    def __str__(self):      #重写函数，设置模型的返回值，只允许返回字符类型的字段
+    def __str__(self):  # 重写函数，设置模型的返回值，只允许返回字符类型的字段
         return str(self.id)
 
-    class Meta:          #重写Meta选项：设置模型的常用属性，有19个
-        verbose_name = '商品类型'           #属性值为字符串，设置模型直观可读的名称并以复数形式表示。
-        verbose_name_plural = '商品类型'    #与verbose_name相同，以单数形式表示。
+    class Meta:  # 重写Meta选项：设置模型的常用属性，有19个
+        verbose_name = '商品类型'  # 属性值为字符串，设置模型直观可读的名称并以复数形式表示。
+        verbose_name_plural = '商品类型'  # 与verbose_name相同，以单数形式表示。
+
 
 class CommodityInfos(models.Model):
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField('商品名称', max_length=100)
-    sizes = models.CharField('颜色规格',max_length=100)
+    sizes = models.CharField('颜色规格', max_length=100)
     types = models.CharField('商品类型', max_length=100)
     price = models.FloatField('商品价格')
     discount = models.FloatField('折后价格')
@@ -35,4 +36,3 @@ class CommodityInfos(models.Model):
     class Meta:
         verbose_name = '商品信息'
         verbose_name_plural = '商品信息'
-
