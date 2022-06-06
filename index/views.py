709318f84616eba30f38061bb115162a4fe0d9c6
HuangYun <hy020316@126.com>
2022-06-06 17:05:53 +0800

    v5.3

--- index/views.py
+++ index/views.py
@@ -5,12 +5,15 @@ from commodity.models import *
 # Create your views here.
 
 def indexView(request):
+    title = '首页'
+    classContent = ' '
+    commodityInfos = CommodityInfos.objects.order_by('-sold').all()[:8]
 
-
-title = '首页'
-classContent = ' '
-commodityInfos = CommodityInfos.objects.order_by('-sold').all()[:8]
-
-types = Types.objects.all()
-cl = [x.seconds for x in types if x.firsts == '儿童服饰']
-clothes = CommodityInfos.objects.filter(types__in=cl).order_by('-sold')[:5]
+    types = Types.objects.all()
+    cl = [x.seconds for x in types if x.firsts == '儿童服饰']
+    clothes = CommodityInfos.objects.filter(types__in=cl).order_by('-sold')[:5]
+    fl = [x.seconds for x in types if x.firsts == '奶粉辅食']
+    food = CommodityInfos.objects.filter(types__in=fl).order_by('-sold')[:5]
+    gl = [x.seconds for x in types if x.firsts == '儿童用品']
+    goods = CommodityInfos.objects.filter(types__in=gl).order_by('-sold')[:5]
+    return render(request, 'index.html', locals())
